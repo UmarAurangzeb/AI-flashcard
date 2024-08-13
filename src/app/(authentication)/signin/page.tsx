0e85d4d7c9272f9a008481fcc4da7e79ce0edc32
umar aurangzeb <umar12598@gmail.com>
2024-08-14 02:41:12 +0500

    authentication,prisma and frontend added for auth,issues with adding bg-colors in frontend

---
+++ src/app/(authentication)/signin/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+import Signin from '@/components/component/Signin';
+export default function page() {
+    return (
+        <>
+            <Signin />
+        </>
+    );
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Homepage } from "@/components/component/homepage";
+import Signup from "@/components/component/Signup";
 import Image from "next/image";
 
 export default function Home() {
-  return(
-    Homepage()
+  return (
+    <>
+      <Signup />
+    </>
   )
 }
